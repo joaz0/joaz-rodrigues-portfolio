53401cd4ee8243c070e38f864264824f9f269bc4
joaz0 <joazrodrigues21@gmail.com>
2025-09-08 14:37:19 -0300

    feat: Reestruturar o HTML e CSS do portfólio, adicionando seções de serviços e projetos, além de melhorias no tema claro/escuro e responsividade

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
-const button = document.getElementById("switch_theme");
-const ChevronDown = document.getElementById("chevron-down");
+const button_theme = document.getElementById("switch_theme");
 
-button.addEventListener("click", () => {
+button_theme.addEventListener("click", () => {
   document.body.classList.toggle("dark");
   document.body.classList.toggle("light");
     if (document.body.classList.contains("dark")) {
-    button.classList.remove("fa-moon");
-    button.classList.add("fa-sun");
+    button_theme.classList.remove("fa-moon");
+    button_theme.classList.add("fa-sun");
     logo.src = "Imagens/Logo/Logo_dark.png";
   } else {
-    button.classList.remove("fa-sun");
-    button.classList.add("fa-moon");
+    button_theme.classList.remove("fa-sun");
+    button_theme.classList.add("fa-moon");
     logo.src = "Imagens/Logo/Logo_light.png";
   }
 });
+
+
+const ChevronDown = document.getElementById("chevron-down");
 
 ChevronDown.addEventListener("click", () => {
   window.scrollBy({
     top: window.innerHeight,
     behavior: 'smooth'
+  
   });
 });
+
+
+const button_service = document.getElementById("button_services");
+
+button_service.addEventListener("click", () => {
+  window.location.href = "#services";
+});
+
+const button_contact = document.getElementById("button_contact");
+
+button_contact.addEventListener("click", () => {
+  window.location.href = "#contact";
+});
+
